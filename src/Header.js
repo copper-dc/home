a8a6fe8161b5996dfc3af98ba6b3fe22fc4d428f
JALALUDEENZUBAIR <jalaludeenzubair15@gmail.com>
2024-10-11 08:01:23 +0530

    nav-inprogress

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,25 @@
 import "./Header.css"
+import { FaBarsStaggered } from "react-icons/fa6";
+
 const Header = () => {
+  const toggle = () => {
+    
+  }
   return (
-    <div className="header-section">
-        <span className="logo">COPPER</span>
-        <ul>
-          <li><a>Home</a></li>
-          <li><a>Commands</a></li>
-          <li><a>Features</a></li>
-          <li><a>Supports</a></li>
-        </ul>
-    </div>
+    <>
+      <div className="header-section">
+          <span className="logo">COPPER</span>
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>Commands</a></li>
+            <li><a>Features</a></li>
+            <li><a>Supports</a></li>
+          </ul>
+      </div>
+      <span className="toggle-btn" onClick={()=>toggle()}>
+        <FaBarsStaggered />
+      </span>
+    </>
   )
 }
 
