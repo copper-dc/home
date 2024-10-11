2fde0188796d964d8d7c0a80a9f9200730b03c83
JALALUDEENZUBAIR <jalaludeenzubair15@gmail.com>
2024-10-11 08:23:42 +0530

    nav-added

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,18 @@
 import "./Header.css"
+import { useState } from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
 
 const Header = () => {
+  const [isActive,setISActive]=useState(false);
   const toggle = () => {
-    
+    setISActive(!isActive);
+    console.log(isActive);
   }
   return (
     <>
-      <div className="header-section">
+      <div className= {isActive ? "header-section show" : "header-section"}>
           <span className="logo">COPPER</span>
-          <ul>
+          <ul className= {isActive ? "nav-list show" : "nav-list"}>
             <li><a>Home</a></li>
             <li><a>Commands</a></li>
             <li><a>Features</a></li>
