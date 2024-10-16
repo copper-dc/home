a95ddfa50f3e7f5ca2684de10d3405433986d8a0
JALALUDEENZUBAIR <jalaludeenzubair15@gmail.com>
2024-10-16 10:37:54 +0530

    nav added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
 import "./Header.css"
+import { IoLogoGithub } from "react-icons/io";
+import { CiLogin } from "react-icons/ci";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 
 const Header = () => {
+  const toggle = () => {
+    document.getElementById("nav").classList.toggle("show");
+  }
   return (
     <div className="nav-container">
         <h3 className="logo">COPPER</h3>
-        <ul className="nav-bar">
-            <li>Commands</li>
-            <li>Documentation</li>
-            <li>Features</li>
-            <li>Github</li>
-            <li>Login</li>
+        <ul id="nav" className="nav-bar">
+            <li className="link">Commands</li>
+            <li className="link">Documentation</li>
+            <li className="link">Features</li>
+            <li className="link">Support</li>
+            <li className="btn"><IoLogoGithub className="icon"/>Github</li>
+            <li className="btn"><CiLogin className="icon"/>Login</li>
+            <span className="toggle-btn"><IoCloseCircleOutline className="close-btn" onClick={toggle}/></span>
         </ul>
+        <IoMenu className="menu-btn" onClick={toggle}/>
     </div>
   )
 }
